8fb1f13eabaddd6f5e44ed288b3327d7d4cb6ca0
Jordan Black <jblack530@gmail.com>
2019-10-21 12:13:55 -0700

    finished memoization problems

--- Memoization/memoization_project/lib/leet_code_518.js
+++ Memoization/memoization_project/lib/leet_code_518.js
@@ -1,3 +1,20 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+function change(amount, coins, memo = {}) {
+    if (amount === 0) return 1;
+    if (coins.length === 0) return 0;
+
+    let key = `${amount}-${coins.join(',')}`;
+    if (key in memo) return memo[key];
+
+    let coinVal = coins[coins.length - 1];
+
+    let total = 0;
+    for (let quanitity = 0; quanitity <= (amount / coinVal); quanitity++) {
+        total += change(amount - (quanitity * coinVal), coins.slice(0, -1), memo);
+    }
+
+    memo[key] = total;
+    return total;
+}
--- Memoization/memoization_project/lib/problems.js
+++ Memoization/memoization_project/lib/problems.js
@@ -16,7 +16,11 @@
 // lucasNumberMemo(41)  // => 370248451
 // lucasNumberMemo(42)  // => 599074578
 function lucasNumberMemo(n, memo = {}) {
-
+    if (n in memo) return memo[n];
+    if (n === 0) return 2;
+    if (n === 1) return 1;
+    memo[n] = lucasNumberMemo(n-1, memo) + lucasNumberMemo(n-2, memo);
+    return memo[n];
 }
 
 
@@ -34,7 +38,19 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount, memo = {}) {
+    if (amount === 0) return 0;
 
+    if (amount in memo) return memo[amount];
+
+    let num_coins = []
+    coins.forEach(coin => {
+        if (coin <= amount) {
+            num_coins.push(minChange(coins, amount - coin, memo) + 1);
+        }
+    });
+
+    memo[amount] = Math.min(...num_coins);
+    return memo[amount];
 }
 
 
