ad23c18c1d5fb45835b3e508385638b6b776cb45
Jordan Black <jblack530@gmail.com>
2019-10-24 12:14:26 -0700

    naive sorts problems--- bubble sort, selection sort, insertion sort

--- Naive_Sorts/selection_sort_project/lib/selection_sort.js
+++ Naive_Sorts/selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,22 @@
 function swap(arr, index1, index2) {
-
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+    return arr;
 }
 
 function selectionSort(arr) {
+    for( let i = 0; i < arr.length; i++) {
+        let minI = i;
 
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[minI] > arr[j]) {
+                minI = j;
+            }
+        }
+        swap(arr, i , minI);
+    }
+    return arr;
 }
 
 module.exports = {
