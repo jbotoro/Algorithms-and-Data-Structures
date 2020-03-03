46399c4fb4c67935192960509c389ed2f17b12a3
Jordan Black <jblack530@gmail.com>
2020-03-02 20:39:10 -0800

    recursion iterative fibonacci

---
+++ Udemy course/recursionFibonacci.js
@@ -0,0 +1,17 @@
+function fibonacciIterative(n){
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++){
+    arr.push(arr[i - 2] + arr[i -1]);
+  }
+ return arr[n];
+}
+fibonacciIterative(3);
+
+function fibonacciRecursive(n) {
+  if (n < 2){
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive (n - 2)
+}
+
+fibonacciRecursive(6)
