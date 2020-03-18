7cf40f44fc3078dcff064b3dae908d37c6dd62f2
Jordan Black <jblack530@gmail.com>
2020-03-18 13:11:28 -0700

    execel spreadsheet number function

--- Udemy course/topInterviewQuestionsPt3.js
+++ Udemy course/topInterviewQuestionsPt3.js
@@ -32,3 +32,23 @@ var reverseList = function (head) {
     return head;
 };
 
+// Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function (s) {
+    const charCodeBase = 'A'.charCodeAt(0) - 1;
+    const n = s.length;
+    let number = 0;
+
+    /* 
+     * Think of it as base 26. For example,
+     * Column number of "AB" = 1 * 26^1 + 2 * 26^0 
+     */
+    for (let i = 0; i < n; i++)
+        number += (s.charCodeAt(i) - charCodeBase) * Math.pow(26, n - i - 1);
+
+    return number;
+};
