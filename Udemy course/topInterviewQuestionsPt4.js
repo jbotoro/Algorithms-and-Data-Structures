e04f1bd42eee9337d3bcb884aa4afa3c8a59b418
Jordan Black <jblack530@gmail.com>
2020-03-19 13:09:28 -0700

    best time to buy and sell stock function

--- Udemy course/topInterviewQuestionsPt4.js
+++ Udemy course/topInterviewQuestionsPt4.js
@@ -56,3 +56,33 @@ var deleteNode = function(node) {
     node.val = node.next.val
     node.next = node.next.next;
 };
+
+
+
+
+//Say you have an array for which the ith element is the price of a given stock on day i.
+
+//Design an algorithm to find the maximum profit. 
+//You may complete as many transactions as you like (i.e., buy one and sell one 
+//share of the stock multiple times).
+
+//Note: You may not engage in multiple transactions at the same time 
+//(i.e., you must sell the stock before you buy again).
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let profit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        let prev = prices[i - 1];
+        let current = prices[i];
+
+        if (prev < current) {
+            profit += current - prev;
+        }
+    }
+    
+    return profit;
+};
