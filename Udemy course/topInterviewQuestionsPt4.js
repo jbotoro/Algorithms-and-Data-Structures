a8c36e0f9c142fdf52e9bd491192724cd529aa22
Jordan Black <jblack530@gmail.com>
2020-03-19 13:04:32 -0700

    roman numeral to integer function

---
+++ Udemy course/topInterviewQuestionsPt4.js
@@ -0,0 +1,38 @@
+
+//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+//Symbol       Value
+//I             1
+//V             5
+//X             10
+//L             50
+//C             100
+//D             500
+//M             1000
+
+//Given a roman numeral, convert it to an integer. 
+//Input is guaranteed to be within the range from 1 to 3999.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+symbols = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+
+var romanToInt = function(s) {
+  value = 0;
+  for (let i = 0; i < s.length; i += 1) {
+    symbols[s[i]] < symbols[s[i + 1]]
+      ? (value -= symbols[s[i]])
+      : (value += symbols[s[i]]);
+  }
+  return value;
+};
