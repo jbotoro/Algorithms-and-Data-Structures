3266c54fd9e3a93bac764116fbacce2643b77200
Jordan Black <jblack530@gmail.com>
2020-03-19 13:06:56 -0700

    delete node in a linked list

--- Udemy course/topInterviewQuestionsPt4.js
+++ Udemy course/topInterviewQuestionsPt4.js
@@ -36,3 +36,23 @@ var romanToInt = function(s) {
   }
   return value;
 };
+
+
+// Write a function to delete a node (except the tail) 
+// in a singly linked list, given only access to that node.
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+   
+    node.val = node.next.val
+    node.next = node.next.next;
+};
