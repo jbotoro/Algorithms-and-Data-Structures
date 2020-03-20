9f591d68c4f44d1548cd9cad68d0e73c15021dab
Jordan Black <jblack530@gmail.com>
2020-03-20 13:04:33 -0700

    pascals triangle function

--- Udemy course/topInterviewQuestionsPt5.js
+++ Udemy course/topInterviewQuestionsPt5.js
@@ -56,3 +56,23 @@ function sumOfSquares(numString) {
         return sum + Math.pow(num, 2);
     }, 0);
 }
+
+// #3 Pascal's Triangle:
+//Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+    var pascal = [];
+    for (var i = 0; i < numRows; i++) {
+        pascal[i] = [];
+        pascal[i][0] = 1;
+        for (var j = 1; j < i; j++) {
+            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+        }
+        pascal[i][i] = 1;
+    }
+    return pascal;
+};
