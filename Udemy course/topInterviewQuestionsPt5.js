4cb89b7791ae1720dc6792e57c01b598c3eb2000
Jordan Black <jblack530@gmail.com>
2020-03-20 13:05:56 -0700

    climbing stairs function

--- Udemy course/topInterviewQuestionsPt5.js
+++ Udemy course/topInterviewQuestionsPt5.js
@@ -76,3 +76,26 @@ var generate = function (numRows) {
     }
     return pascal;
 };
+
+
+// #4 Climbing Stairs:
+
+//You are climbing a stair case. It takes n steps to reach to the top.
+//Each time you can either climb 1 or 2 steps.In how many distinct ways can 
+//you climb to the top ?
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    let table = new Array(n + 1);
+    table[0] = 1;
+    table[1] = 1;
+
+    for (let i = 2; i < table.length; i++) {
+        table[i] = table[i - 1] + table[i - 2];
+    }
+
+    return table[table.length - 1];
+};
