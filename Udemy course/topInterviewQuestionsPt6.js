9fe671d198d4a9139474bcffe65e80c7c88a7d2d
Jordan Black <jblack530@gmail.com>
2020-03-23 11:02:55 -0700

    max subarray function

--- Udemy course/topInterviewQuestionsPt6.js
+++ Udemy course/topInterviewQuestionsPt6.js
@@ -32,3 +32,25 @@ var isSymmetric = function (root) {
     // traverse
     return isMirrored(root.left, root.right);
 };
+
+
+// #2 Max SubArray 
+// Given an integer array nums, find the contiguous subarray 
+//(containing at least one number) which has the largest sum and return its sum.
+
+//Example:
+
+//Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4],
+//    Output: 6
+// Explanation: [4, -1, 2, 1] has the largest sum = 6.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i], nums[i] + nums[i - 1])
+    }
+    return Math.max(...nums)
+};
