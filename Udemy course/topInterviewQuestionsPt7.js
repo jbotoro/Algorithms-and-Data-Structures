d7a1e492bca95420d499cbbcebfb00ea25fc669e
Jordan Black <jblack530@gmail.com>
2020-03-24 10:47:55 -0700

    two sum function

--- Udemy course/topInterviewQuestionsPt7.js
+++ Udemy course/topInterviewQuestionsPt7.js
@@ -32,3 +32,37 @@ var plusOne = function (digits) {
     digits.unshift(1);
     return digits;
 };
+
+//#2 Two Sum
+
+//Given an array of integers, return indices of the two numbers such that they 
+//add up to a specific target.
+
+//You may assume that each input would have exactly one solution, and you may 
+//not use the same element twice.
+
+//    Example:
+
+//Given nums = [2, 7, 11, 15], target = 9,
+
+//    Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(target - nums[i])) {
+            return [map.get(target - nums[i]), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+    return [];
+};
