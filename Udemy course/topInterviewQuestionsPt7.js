a62739993db259692b7d0891b9350f6ea4127121
Jordan Black <jblack530@gmail.com>
2020-03-24 11:06:17 -0700

    remove duplicates from sorted array function

--- Udemy course/topInterviewQuestionsPt7.js
+++ Udemy course/topInterviewQuestionsPt7.js
@@ -119,3 +119,33 @@ function countChars(string) {
     }
     return result;
 }
+
+//#4 Remove Duplicates from Sorted Array
+
+//Given a sorted array nums, remove the duplicates in-place such that each 
+//element appear only once and return the new length.
+
+//Do not allocate extra space for another array, you must do this by modifying 
+//the input array in -place with O(1) extra memory.
+
+//    Example 1:
+
+//Given nums = [1, 1, 2],
+
+//    Your function should return length = 2, with the first two elements of 
+//nums being 1 and 2 respectively.
+
+//It doesn't matter what you leave beyond the returned length.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] != nums[i])
+            nums[++i] = nums[j];
+    }
+    return ++i;
+};
