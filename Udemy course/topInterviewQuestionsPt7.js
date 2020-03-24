1da54d96e98f485bd3170f2365eb6d1ddf65da82
Jordan Black <jblack530@gmail.com>
2020-03-24 11:46:46 -0700

    trailing zeroes function

--- Udemy course/topInterviewQuestionsPt7.js
+++ Udemy course/topInterviewQuestionsPt7.js
@@ -149,3 +149,26 @@ var removeDuplicates = function (nums) {
     }
     return ++i;
 };
+
+
+//#5 Factorial Trailing Zeroes
+
+//Given an integer n, return the number of trailing zeroes in n!.
+
+//Example 1:
+
+//Input: 3
+//Output: 0
+//Explanation: 3! = 6, no trailing zero.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function (n) {
+    let numZeroes = 0;
+    for (let i = 5; i <= n; i *= 5) {
+        numZeroes += Math.floor(n / i);
+    }
+    return numZeroes;
+};
