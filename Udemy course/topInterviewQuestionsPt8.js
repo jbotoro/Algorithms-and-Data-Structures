cac7b1c2574393740f1ca2834fbe3489bef5cbf4
Jordan Black <jblack530@gmail.com>
2020-03-25 10:50:26 -0700

    min stack function

--- Udemy course/topInterviewQuestionsPt8.js
+++ Udemy course/topInterviewQuestionsPt8.js
@@ -169,5 +169,71 @@ var merge = function (nums1, m, nums2, n) {
     nums1.sort(sortNums)
 };
 
+//#5 Min Stack
 
+//Design a stack that supports push, pop, top, and retrieving the minimum element 
+//in constant time.
+
+//push(x)-- Push element x onto stack.
+//    pop()-- Removes the element on top of the stack.
+//        top()-- Get the top element.
+//            getMin()-- Retrieve the minimum element in the stack.
+
+
+//                Example:
+
+//MinStack minStack = new MinStack();
+//minStack.push(-2);
+//minStack.push(0);
+//minStack.push(-3);
+//minStack.getMin(); --> Returns - 3.
+//minStack.pop();
+//minStack.top(); --> Returns 0.
+//minStack.getMin(); --> Returns - 2.
+
+var MinStack = function () {
+    this.elements = [];
+};
+
+/**
+
+ @param {number} x
+ @return {void}
+ */
+MinStack.prototype.push = function (x) {
+    this.elements.push({
+        value: x,
+        min: this.elements.length === 0 ? x : Math.min(x, this.getMin()),
+    });
+};
+/**
+
+ @return {void}
+ */
+MinStack.prototype.pop = function () {
+    this.elements.pop();
+};
+/**
+
+ @return {number}
+ */
+MinStack.prototype.top = function () {
+    return this.elements[this.elements.length - 1].value;
+};
+/**
+
+ @return {number}
+ */
+MinStack.prototype.getMin = function () {
+    return this.elements[this.elements.length - 1].min;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
 
