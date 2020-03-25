bb87725c572e19a52b8dfdc8b3960e5a4340bf45
Jordan Black <jblack530@gmail.com>
2020-03-25 10:35:53 -0700

    palindrome linked list

--- Udemy course/topInterviewQuestionsPt8.js
+++ Udemy course/topInterviewQuestionsPt8.js
@@ -83,4 +83,55 @@ var hasCycle = function (head) {
 };
 
 
+//#3 Palindrome Linked List
+
+//Given a singly linked list, determine if it is a palindrome.
+
+//Example 1:
+
+//Input: 1 -> 2
+//Output: false
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+    if (head == null || head.next == null) return true;
+    var slow = head;
+    var fast = head;
+    while (fast.next != null && fast.next.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    slow.next = reverseList(slow.next);
+    slow = slow.next;
+    while (slow != null) {
+        if (head.val != slow.val) return false;
+        head = head.next;
+        slow = slow.next;
+    }
+    return true;
+};
+
+var reverseList = function (head) {
+    var pre = null;
+    var next = null;
+    while (head != null) {
+        next = head.next;
+        head.next = pre;
+        pre = head;
+        head = next;
+    }
+    return pre;
+};
+
+
 
