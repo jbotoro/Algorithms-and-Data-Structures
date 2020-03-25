ac062bc11a72da9de5fe849c7e894284395a82e1
Jordan Black <jblack530@gmail.com>
2020-03-25 10:32:59 -0700

    linked list cycle function

--- Udemy course/topInterviewQuestionsPt8.js
+++ Udemy course/topInterviewQuestionsPt8.js
@@ -40,3 +40,47 @@ var isValid = function (s) {
     return stack.length === 0;
 };
 
+
+//#2 Linked List Cycle
+
+//Given a linked list, determine if it has a cycle in it.
+
+//To represent a cycle in the given linked list, we use an integer pos which 
+//represents the position(0 - indexed) in the linked list where tail connects 
+//to.If pos is - 1, then there is no cycle in the linked list.
+
+//Example 1:
+
+//Input: head = [3, 2, 0, -4], pos = 1
+//Output: true
+//Explanation: There is a cycle in the linked list, where tail connects to the 
+//second node.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    const set = new Set();
+    let node = head;
+    // return true when node has been visited and saved in the set
+    while (node !== null) {
+        if (set.has(node)) {
+            return true;
+        }
+        set.add(node);
+        node = node.next
+    }
+    return false;
+};
+
+
+
