fe2c8fccfb72bafd877ce01c0ebf9ad2622d6967
Jordan Black <jblack530@gmail.com>
2020-03-25 10:45:12 -0700

    merge sorted array

--- Udemy course/topInterviewQuestionsPt8.js
+++ Udemy course/topInterviewQuestionsPt8.js
@@ -133,5 +133,41 @@ var reverseList = function (head) {
     return pre;
 };
 
+//#4 Merge Sorted Array
+
+//Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+//    Note:
+
+//The number of elements initialized in nums1 and nums2 are m and n respectively.
+//You may assume that nums1 has enough space(size that is greater or equal to m + n) to hold additional elements from nums2.
+//    Example:
+
+//Input:
+//nums1 = [1, 2, 3, 0, 0, 0], m = 3
+//nums2 = [2, 5, 6], n = 3
+
+//Output: [1, 2, 2, 3, 5, 6]
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+const sortNums = (a, b) => {
+    return a - b;
+}
+
+var merge = function (nums1, m, nums2, n) {
+
+    for (let i = m; i < nums1.length; i++) {
+        nums1[i] = nums2.shift();
+    }
+    nums1.sort(sortNums)
+};
+
 
 
