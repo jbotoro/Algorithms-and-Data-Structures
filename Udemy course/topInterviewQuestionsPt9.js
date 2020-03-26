c913c066e02c152ab1f64d9c79f839edd9a94888
Jordan Black <jblack530@gmail.com>
2020-03-26 11:13:37 -0700

    reverse bits function

--- Udemy course/topInterviewQuestionsPt9.js
+++ Udemy course/topInterviewQuestionsPt9.js
@@ -173,3 +173,33 @@ var mySqrt = function (x) {
     }
     return right;
 };
+
+//#5 Reverse Bits
+
+//Reverse bits of a given 32 bits unsigned integer.
+
+
+
+//Example 1:
+
+//Input: 00000010100101000001111010011100
+//Output: 00111001011110000010100101000000
+//Explanation: The input binary string 00000010100101000001111010011100 represents 
+//the unsigned integer 43261596, so return 964176192 which its binary representation 
+//is 00111001011110000010100101000000.
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function (n) {
+    var result = 0;
+    var count = 32;
+
+    while (count--) {
+        result *= 2;
+        result += n & 1;
+        n = n >> 1;
+    }
+    return result;
+};
