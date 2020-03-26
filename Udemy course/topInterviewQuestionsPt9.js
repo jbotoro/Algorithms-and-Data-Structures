5935f1866d0583808bf34b0dc442bec232728cfb
Jordan Black <jblack530@gmail.com>
2020-03-26 11:05:29 -0700

    sqrt(x) function

--- Udemy course/topInterviewQuestionsPt9.js
+++ Udemy course/topInterviewQuestionsPt9.js
@@ -126,3 +126,50 @@ var strStr = function (haystack, needle) {
     }
     return -1;
 };
+
+//#4 Sqrt(x)
+
+//Implement int sqrt(int x).
+
+//Compute and return the square root of x, where x is guaranteed to be a 
+//non - negative integer.
+
+//Since the return type is an integer, the decimal digits are truncated and only 
+//the integer part of the result is returned.
+
+//    Example 1:
+
+//Input: 4
+//Output: 2
+
+//Remember that the square root of each X must happen within [1, x/2 + 1]
+
+//So we can cut half at the first place.
+
+//The tricky point is that we will return right if there is no square root found.
+
+//The reason is the square root is between[n - 1, n] and we can know that 
+//when while is broken right must be n - 1
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    var left = 1;
+    var right = Math.floor(x / 2) + 1;
+    var mid;
+
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+
+        if (mid * mid > x) {
+            right = mid - 1;
+        } else if (mid * mid < x) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return right;
+};
