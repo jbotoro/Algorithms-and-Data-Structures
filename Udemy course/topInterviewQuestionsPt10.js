5e121e5de6894252997be12add02011e860d1986
Jordan Black <jblack530@gmail.com>
2020-03-27 11:34:19 -0700

    reverse integer function

--- Udemy course/topInterviewQuestionsPt10.js
+++ Udemy course/topInterviewQuestionsPt10.js
@@ -72,3 +72,21 @@ var countPrimes = function(n) {
     }
     return nums.filter(n => n).length;
 };
+
+//#4 Reverse Integer
+
+//Given a 32-bit signed integer, reverse digits of an integer.
+
+//Example 1:
+
+//Input: 123
+//Output: 321
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    const reversed =  parseInt(Math.abs(x).toString().split('').reverse().join('')) * Math.sign(x);
+    return (reversed <= 0x7fffffff && reversed >= -0x80000000) ? reversed : 0;
+};
