030640ccf552dd661bf6dbdce6b0c3df3dd14b91
Jordan Black <jblack530@gmail.com>
2020-03-27 11:28:21 -0700

    rotate array function

--- Udemy course/topInterviewQuestionsPt10.js
+++ Udemy course/topInterviewQuestionsPt10.js
@@ -22,3 +22,27 @@ var isPalindrome = function(s) {
     // make sure both are lower cased
     return strippedString.toLowerCase() == reversedString.toLowerCase();
 };
+
+
+//#2 Rotate Array
+
+//Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+//Example 1:
+
+//Input: [1,2,3,4,5,6,7] and k = 3
+//Output: [5,6,7,1,2,3,4]
+//Explanation:
+//rotate 1 steps to the right: [7,1,2,3,4,5,6]
+//rotate 2 steps to the right: [6,7,1,2,3,4,5]
+//rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    nums.unshift(...nums.splice(nums.length - k));
+    return nums;
+};
