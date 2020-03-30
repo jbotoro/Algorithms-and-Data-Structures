f632206e09a6b89204be0a22a0150d77cd3eb489
Jordan Black <jblack530@gmail.com>
2020-03-30 11:33:19 -0700

    number of islands function

--- Udemy course/topAmazonProblems.js
+++ Udemy course/topAmazonProblems.js
@@ -23,3 +23,63 @@ function lengthOfLongestSubstring(s) {
     }, 0);
 }
 
+//#2 Number of Islands
+
+//Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+//An island is surrounded by water and is formed by connecting adjacent lands 
+//horizontally or vertically. You may assume all four edges of the grid are all 
+//surrounded by water.
+
+//Example 1:
+
+//Input:
+//11110
+//11010
+//11000
+//00000
+
+//Output: 1
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    let count = 0;
+    
+    function depthSearch(x, y) {
+        if (grid[x][y] === '1') {
+            grid[x][y] = '0';
+        } else {
+            return;
+        }
+
+        if (x < grid.length - 1) {
+            depthSearch(x+1, y);
+        }
+        
+        if (y < grid[x].length - 1) {
+            depthSearch(x, y+1);
+        }
+        
+        if (x > 0 && x < grid.length) {
+            depthSearch(x-1, y);
+        }
+        
+        if (y > 0 && y < grid[x].length) {
+            depthSearch(x, y-1);
+        }
+    }
+    
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === '1') {
+                count++;
+                depthSearch(i, j);
+            }
+        }
+    }
+    
+    return count;
+};
+
