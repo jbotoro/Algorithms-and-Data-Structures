3839e0533ee714e5d60b6e5acb49bfd67538f5e2
Jordan Black <jblack530@gmail.com>
2020-03-31 11:46:35 -0700

    next permutation function

--- Udemy course/topGoogleProblems.js
+++ Udemy course/topGoogleProblems.js
@@ -83,3 +83,42 @@ var wordBreak = function(s, wordDict) {
     } 
     return dp[s.length]
 };
+
+//#3 Next Permutation
+
+//Implement next permutation, which rearranges numbers into the lexicographically 
+//next greater permutation of numbers.
+
+//If such arrangement is not possible, it must rearrange it as the lowest 
+//possible order (ie, sorted in ascending order).
+
+//The replacement must be in-place and use only constant extra memory.
+
+//Here are some examples. Inputs are in the left-hand column and its 
+//corresponding outputs are in the right-hand column.
+
+//1,2,3 → 1,3,2
+//3,2,1 → 1,2,3
+//1,1,5 → 1,5,1
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    let j = nums.length - 1, i = j - 1;
+    while (nums[i + 1] <= nums[i]) i--;
+    if (~i) {
+        while (nums[j] <= nums[i]) j--;
+        swap(nums, i, j);
+    }
+    for (let k = i + 1, stop = (i + nums.length) / 2; k < stop; k++) {
+        swap(nums, k, nums.length - k + i);
+    }
+};
+
+function swap(nums, i, j) {
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
