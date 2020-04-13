04dc8d09fe19ea3d1c4b8325c7a1654ff99170ab
Jordan Black <jblack530@gmail.com>
2020-04-13 14:22:02 -0700

    valid sudoku algo

--- Udemy course/leetCodeProblemsEasyPt1.js
+++ Udemy course/leetCodeProblemsEasyPt1.js
@@ -29,3 +29,60 @@ const letterCombinations = (digits, i = 0) => {
         return outputArray // move onto next reduce loop (next "thisLetter")
     }, []) // default & initial outputArray value
 }
+
+// Valid Sudoku
+
+//Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be 
+//validated according to the following rules:
+
+//Each row must contain the digits 1-9 without repetition.
+//Each column must contain the digits 1-9 without repetition.
+//Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without 
+//repetition.
+
+//Input:
+//[
+//  ["5","3",".",".","7",".",".",".","."],
+//  ["6",".",".","1","9","5",".",".","."],
+//  [".","9","8",".",".",".",".","6","."],
+//  ["8",".",".",".","6",".",".",".","3"],
+//  ["4",".",".","8",".","3",".",".","1"],
+//  ["7",".",".",".","2",".",".",".","6"],
+//  [".","6",".",".",".",".","2","8","."],
+//  [".",".",".","4","1","9",".",".","5"],
+//  [".",".",".",".","8",".",".","7","9"]
+//]
+//Output: true
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    for (let i = 0; i < 9; i++) {
+        let row = new Set(),
+            col = new Set(),
+            box = new Set();
+
+            for (let j = 0; j < 9; j++) {
+            let _row = board[i][j];
+            let _col = board[j][i];
+            let _box = board[3*Math.floor(i/3)+Math.floor(j/3)][3*(i%3)+(j%3)]
+            
+            if (_row != '.') {
+                if (row.has(_row)) return false;
+                row.add(_row);
+            }
+            if (_col != '.') {
+                if (col.has(_col)) return false;
+                col.add(_col);
+            }
+            
+                if (_box != '.') {
+                    if (box.has(_box)) return false;
+                    box.add(_box);
+                } 
+            }
+        }
+    return true
+};
