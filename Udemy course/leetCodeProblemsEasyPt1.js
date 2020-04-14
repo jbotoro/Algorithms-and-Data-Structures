74d66b5bc22cfeaf8ecfe02fa3eee474b2f36b32
Jordan Black <jblack530@gmail.com>
2020-04-14 14:05:53 -0700

    unique paths algo leetcode

--- Udemy course/leetCodeProblemsEasyPt1.js
+++ Udemy course/leetCodeProblemsEasyPt1.js
@@ -134,3 +134,42 @@ var permute = function(nums) {
     backtrack(0);
     return output;
 };
+
+//Unique Paths
+
+//A robot is located at the top-left corner of a m x n grid (marked 'Start' 
+//in the diagram below).
+
+//The robot can only move either down or right at any point in time. 
+//The robot is trying to reach the bottom-right corner of the grid 
+//(marked 'Finish' in the diagram below).
+
+//How many possible unique paths are there?
+
+//Example 1:
+
+//Input: m = 3, n = 2
+//Output: 3
+//Explanation:
+//From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+//1. Right -> Right -> Down
+//2. Right -> Down -> Right
+//3. Down -> Right -> Right
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let carry = [];
+    for(let i = 1; i <= m; i++) {
+        for(let j = 1; j <= n; j++){
+            if (i === 1 || j === 1)
+                carry[j] = 1;
+            if (i !== 1&& j !== 1)
+                carry[j] += carry[j - 1];
+        }
+    }
+    return carry[n];
+};
