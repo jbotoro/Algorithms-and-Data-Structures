64e37769c5c71094ae1729876f15512b1b93693e
Jordan Black <jblack530@gmail.com>
2020-04-16 13:07:15 -0700

    find median of two sorted arrays problem :

--- Udemy course/leetCodeProblemsEasyPt1.js
+++ Udemy course/leetCodeProblemsEasyPt1.js
@@ -352,3 +352,44 @@ var removeNthFromEnd = function(head, n) {
     }
     return head;
 };
+
+//#8 Median of Two Sorted Arrays
+
+//There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+//Find the median of the two sorted arrays. The overall run time complexity 
+//should be O(log (m+n)).
+
+//You may assume nums1 and nums2 cannot be both empty.
+
+//Example 1:
+
+//nums1 = [1, 3]
+//nums2 = [2]
+
+//The median is 2.0
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    const total = nums1.length + nums2.length;
+    const limit = Math.floor(total / 2) + 1;
+    let i = 0, j = 0, prev, last
+
+    while(i + j < limit) {
+        if (last !== undefined) {
+        prev = last;
+        }
+        if (nums1[i] < nums2[j] || j === nums2.length) {
+            last = nums1[i++];
+        } else {
+            last = nums2[j++];
+        }
+    }
+
+    return total % 2 === 0 ? (prev + last) / 2 : last;
+};
