d2221ed31b9ca9a5f307e33dfc638abef52de49c
Jordan Black <jblack530@gmail.com>
2020-04-17 13:21:38 -0700

    find all numbers disappeared in an array problem

--- Udemy course/leetCodeProblemsEasyPt1.js
+++ Udemy course/leetCodeProblemsEasyPt1.js
@@ -393,3 +393,38 @@ var findMedianSortedArrays = function(nums1, nums2) {
 
     return total % 2 === 0 ? (prev + last) / 2 : last;
 };
+
+
+//#9 Find All Numbers Disappeared in an Array
+
+//Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), 
+//some elements appear twice and others appear once.
+
+//Find all the elements of [1, n] inclusive that do not appear in this array.
+
+//Could you do it without extra space and in O(n) runtime? You may assume the 
+//returned list does not count as extra space.
+
+//Example:
+
+//Input:
+//[4,3,2,7,8,2,3,1]
+
+//Output:
+//[5,6]
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    // Unique elements array
+    let newArr = [...new Set(nums)];
+
+    // Full elements array
+    let fullArr = Array.from(new Array(nums.length + 1).keys()).slice(1);
+    
+    return fullArr.filter(item=>{
+        return newArr.indexOf(item) === -1
+    });
+};
