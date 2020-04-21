cb563d525a2b948c16a7a6a749c457fce6b9cf76
Jordan Black <jblack530@gmail.com>
2020-04-21 12:23:10 -0700

    top  k frequent elements leetcode

--- Udemy course/leetCodeProblemsEasyPt1.js
+++ Udemy course/leetCodeProblemsEasyPt1.js
@@ -547,3 +547,53 @@ var countSubstrings = function(s) {
     }
     return count1
 }
+
+
+//#13 Top K Frequent Elements
+
+//Given a non-empty array of integers, return the k most frequent elements.
+
+//Example 1:
+
+//Input: nums = [1,1,1,2,2,3], k = 2
+//Output: [1,2]
+//Example 2:
+
+//Input: nums = [1], k = 1
+//Output: [1]
+//Note:
+
+//You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+//Your algorithm's time complexity must be better than O(n log n), where n is 
+//the array's size.
+//It's guaranteed that the answer is unique, in other words the set of the top k 
+//frequent elements is unique.
+//You can return the answer in any order.
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    /**
+     * The goal is to build a map that associates the numbers 
+     * with their occurences. With the map, we can transform 
+     * it into a sorted array in descending order and return 
+     * the first k elements.
+     * 
+     * Time complexity: O(nlog(n))
+     * Space complexity: O(n)
+     */
+    const map = nums.reduce((map, num) => {
+        const occurence = map.get(num) || 0;
+        map.set(num, occurence + 1);
+        return map;
+    }, new Map());
+    
+    return Array.from(map)
+        .sort((a, b) => b[1] - a[1])
+        .map(item => item[0])
+        .slice(0, k);
+};
