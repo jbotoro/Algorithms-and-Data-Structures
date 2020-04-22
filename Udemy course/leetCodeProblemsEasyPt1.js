7df6e75d0393ad13ab2b607722c6bba19bbd4129
Jordan Black <jblack530@gmail.com>
2020-04-22 14:42:53 -0700

    search 2d matrix leetcode

--- Udemy course/leetCodeProblemsEasyPt1.js
+++ Udemy course/leetCodeProblemsEasyPt1.js
@@ -679,3 +679,47 @@ var decodeString = function(s) {
     }
     return s;
 };
+
+
+//#15 Search a 2D Matrix II
+
+//Write an efficient algorithm that searches for a value in an m x n matrix. 
+//This matrix has the following properties:
+
+//Integers in each row are sorted in ascending from left to right.
+//Integers in each column are sorted in ascending from top to bottom.
+//Example:
+
+//Consider the following matrix:
+
+//[
+//  [1,   4,  7, 11, 15],
+//  [2,   5,  8, 12, 19],
+//  [3,   6,  9, 16, 22],
+//  [10, 13, 14, 17, 24],
+//  [18, 21, 23, 26, 30]
+//]
+//Given target = 5, return true.
+
+//Given target = 20, return false.
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    const m = matrix.length;
+    const n = m > 0 ? matrix[0].length : 0;
+
+    let i = m - 1, j = 0;
+    while (i >= 0 && j < n) {
+        if (matrix[i][j] === target) return true;
+        if (matrix[i][j] > target) {
+            i--;
+        } else {
+            j++;
+        }
+    }
+    return false;
+};
