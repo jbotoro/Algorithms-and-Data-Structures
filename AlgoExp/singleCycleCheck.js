9bb91debec0ae447894ad35f4817dde99a2b3928
Jordan Black <jblack530@gmail.com>
2020-05-06 15:36:34 -0700

    single cycle check algo

---
+++ AlgoExp/singleCycleCheck.js
@@ -0,0 +1,17 @@
+function hasSingleCycle(array) {
+  // Write your code here.
+	let numElementsVisited = 0;
+	let currentIdx = 0;
+	while(numElementsVisited < array.length){
+		if (numElementsVisited > 0 && currentIdx === 0) return false;
+		numElementsVisited++;
+		currentIdx = getNextIdx(currentIdx, array);
+	}
+	return currentIdx === 0;
+}
+
+function getNextIdx(currentIdx, array) {
+	const jump = array[currentIdx];
+	const nextIdx = (currentIdx + jump) % array.length;
+	return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
+}
