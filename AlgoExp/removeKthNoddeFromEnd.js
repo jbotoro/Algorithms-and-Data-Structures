4aee76ca03b373ac2abccbf0e32c47ec7659e7e5
Jordan Black <jblack530@gmail.com>
2020-05-07 17:11:43 -0700

    remove Kth Node From End

---
+++ AlgoExp/removeKthNoddeFromEnd.js
@@ -0,0 +1,29 @@
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeKthNodeFromEnd(head, k) {
+  // Write your code here.
+	let counter = 1;
+	let first = head;
+	let second = head;
+	while (counter <= k) {
+		second = second.next;
+		counter++;
+	}
+	if(second === null){
+		head.value = head.next.value;
+		head.next = head.next.next;
+		return;
+	}
+	
+	while(second.next !== null){
+		second = second.next;
+		first = first.next;
+	}
+	first.next = first.next.next;
+}
