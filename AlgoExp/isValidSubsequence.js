ccce87344f4f2e5067803232cbb5a5c352522ac9
Jordan Black <jblack530@gmail.com>
2020-05-12 16:57:42 -0700

    is valid subsequence algo

---
+++ AlgoExp/isValidSubsequence.js
@@ -0,0 +1,10 @@
+function isValidSubsequence(array, sequence) {
+  // Write your code here.
+	let arrIdx = 0;
+	let seqIdx = 0;
+	while(arrIdx < array.length && seqIdx < sequence.length){
+		if(array[arrIdx] === sequence[seqIdx]) seqIdx++;
+		arrIdx++;
+	}
+	return seqIdx === sequence.length;
+}
