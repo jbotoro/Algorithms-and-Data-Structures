07463b01e6a0feb79594e6e45d80305e52395a1b
Jordan Black <jblack530@gmail.com>
2020-05-22 15:00:51 -0700

    balanced brackets algo

---
+++ AlgoExp/balancedBrackets.js
@@ -0,0 +1,22 @@
+function balancedBrackets(string) {
+  // Write your code here.
+	const openingBrackets = '([{';
+	const closingBrackets = ')]}';
+	const matchingBrackets = {')' : '(', ']' : '[', '}' : '{'};
+	const stack = []
+	for(const char of string){
+		if(openingBrackets.includes(char)){
+			stack.push(char);
+		} else if (closingBrackets.includes(char)){
+			if(stack.length === 0){
+				return false;
+			}
+			if(stack[stack.length - 1] === matchingBrackets[char]){
+				stack.pop();
+			} else {
+				return false
+			}
+		}
+	}
+	return stack.length === 0;
+}
