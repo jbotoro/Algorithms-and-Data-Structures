befb5b7168c2be00c559c8c882e67e02454363be
Jordan Black <jblack530@gmail.com>
2020-06-02 14:51:17 -0700

    max sum increasing subsequence

---
+++ AlgoExp/maxSumIncreasingSubsequence.js
@@ -0,0 +1,27 @@
+function maxSumIncreasingSubsequence(array) {
+  // Write your code here.
+	const sequences = new Array(array.length);
+	const sums = array.map(num => num);
+	let maxSumIdx = 0;
+	for(let i = 0; i < array.length; i++){
+		const currentNum = array[i];
+		for(let j = 0; j < i; j++){
+			const otherNum = array[j];
+			if(otherNum < currentNum && sums[j] + currentNum >= sums[i]){
+				sums[i] = sums[j] + currentNum;
+				sequences[i] = j
+			}
+		}
+		if(sums[i] >= sums[maxSumIdx]) maxSumIdx = i;
+	}
+	return [sums[maxSumIdx], buildSequence(array, sequences, maxSumIdx)];
+}
+
+function buildSequence(array, sequences, currentIdx){
+	const sequence = [];
+	while (currentIdx !== undefined){
+		sequence.unshift(array[currentIdx]);
+		currentIdx = sequences[currentIdx]
+	}
+	return sequence
+}
