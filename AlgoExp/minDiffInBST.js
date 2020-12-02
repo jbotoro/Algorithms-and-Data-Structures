c24950ab8e5850e1dbfa81781263c6136c5856d5
Jordan Black <jblack530@gmail.com>
2020-12-01 19:24:00 -0800

    min diff in bst algo

---
+++ AlgoExp/minDiffInBST.js
@@ -0,0 +1,47 @@
+// Given a Binary Search Tree (BST) with the root node root, return the minimum difference between the values of any two different nodes in the tree.
+
+// Example :
+
+// Input: root = [4,2,6,1,3,null,null]
+// Output: 1
+// Explanation:
+// Note that root is a TreeNode object, not an array.
+
+// The given tree [4,2,6,1,3,null,null] is represented by the following diagram:
+
+//           4
+//         /   \
+//       2      6
+//      / \    
+//     1   3  
+
+// while the minimum difference in this tree is 1, it occurs between node 1 and node 2, also between node 3 and node 2.
+// Note:
+
+// The size of the BST will be between 2 and 100.
+// The BST is always valid, each node's value is an integer, and each node's value is different.
+// This question is the same as 530: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+
+
+var minDiffInBST = function(root) {
+    let min = Infinity;
+    
+    const recurse = (node) => {
+        if (!node) return;    
+        getMin(node.val, node.left)
+        getMin(node.val, node.right);
+        recurse(node.left);
+        recurse(node.right);
+    }
+    
+    const getMin = (val, node) => {
+        if (!node) return;
+        min = Math.min(min, Math.abs(val - node.val));
+        getMin(val, node.left);
+        getMin(val, node.right);
+    }
+    
+    recurse(root);
+    
+    return min;
+};
