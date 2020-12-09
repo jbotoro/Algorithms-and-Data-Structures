e1e07fbe587af852908a0471ef116b349355bf21
Jordan Black <jblack530@gmail.com>
2020-12-08 20:26:57 -0800

    relative sort array algo

---
+++ AlgoExp/relativeSortArray.js
@@ -0,0 +1,36 @@
+// Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+// Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.  Elements that don't appear in arr2 should be placed at the end of arr1 in ascending order.
+
+ 
+
+// Example 1:
+
+// Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+// Output: [2,2,2,1,4,3,3,9,6,7,19]
+ 
+
+// Constraints:
+
+// 1 <= arr1.length, arr2.length <= 1000
+// 0 <= arr1[i], arr2[i] <= 1000
+// All the elements of arr2 are distinct.
+// Each arr2[i] is in arr1.
+
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+var relativeSortArray = function(arr1, arr2) {
+    const lookup = new Map();
+    const N = arr2.length;
+    arr2.forEach((a, i) => {
+        lookup.set(a, i);
+    });
+    return arr1.sort((a, b) => {
+        a = lookup.has(a) ? lookup.get(a) : N + a;
+        b = lookup.has(b) ? lookup.get(b) : N + b;
+        return a - b;
+    });
+};
