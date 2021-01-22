7a0df1bd8e7c99221cebcf592546b0a0d78e8938
Jordan Black <jblack530@gmail.com>
2021-01-21 21:04:20 -0800

    furthest building algo

---
+++ AlgoExp/furthestBuilding.js
@@ -0,0 +1,81 @@
+
+// You are given an integer array heights representing the heights of buildings, some bricks, and some ladders.
+
+// You start your journey from building 0 and move to the next building by possibly using bricks or ladders.
+
+// While moving from building i to building i+1 (0-indexed),
+
+// If the current building's height is greater than or equal to the next building's height, you do not need a ladder or bricks.
+// If the current building's height is less than the next building's height, you can either use one ladder or (h[i+1] - h[i]) bricks.
+// Return the furthest building index (0-indexed) you can reach if you use the given ladders and bricks optimally.
+
+ 
+
+// Example 1:
+
+
+// Input: heights = [4,2,7,6,9,14,12], bricks = 5, ladders = 1
+// Output: 4
+// Explanation: Starting at building 0, you can follow these steps:
+// - Go to building 1 without using ladders nor bricks since 4 >= 2.
+// - Go to building 2 using 5 bricks. You must use either bricks or ladders because 2 < 7.
+// - Go to building 3 without using ladders nor bricks since 7 >= 6.
+// - Go to building 4 using your only ladder. You must use either bricks or ladders because 6 < 9.
+// It is impossible to go beyond building 4 because you do not have any more bricks or ladders.
+// Example 2:
+
+// Input: heights = [4,12,2,7,3,18,20,3,19], bricks = 10, ladders = 2
+// Output: 7
+// Example 3:
+
+// Input: heights = [14,3,19,3], bricks = 17, ladders = 0
+// Output: 3
+ 
+
+// Constraints:
+
+// 1 <= heights.length <= 105
+// 1 <= heights[i] <= 106
+// 0 <= bricks <= 109
+// 0 <= ladders <= heights.length
+
+var furthestBuilding = function(heights, bricks, ladders) {
+    if (ladders >= heights.length-1)
+        return heights.length-1;
+    
+    const dp = new Array(heights.length).fill(-Infinity);
+    dp[0] = bricks;
+    
+    let res = 0, diff;
+    for (let i = 0; i <= ladders; i++) {
+        let pre = dp[0];
+        
+        for (let j = 1; j < heights.length; j++) {
+            if (heights[j] <= heights[j-1]) {
+                [pre, dp[j]] = [dp[j], dp[j-1]];
+            } else {
+                diff = heights[j] - heights[j-1];
+                let temp = dp[j];
+                
+                if (i === 0)
+                    dp[j] = dp[j-1] - diff;
+                else
+                    dp[j] = Math.max(dp[j-1]-diff, pre);
+                
+                if (dp[j] < 0)
+                    break;
+                
+                pre = temp;
+                
+            }
+            res = Math.max(j, res);
+        }
+        
+        // Corner case: if we have enough remaining ladders to achieve the end of buildings, return the last index
+        if (res+(ladders-i) >= heights.length-1)
+            return heights.length-1;
+    }
+    
+    
+    return res;
+};
