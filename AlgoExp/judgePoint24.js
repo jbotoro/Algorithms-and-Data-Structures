594e05a08a7398629f8f6854008071ad433c1d95
Jordan Black <jblack530@gmail.com>
2021-03-08 15:02:32 -0800

    24 game algo

---
+++ AlgoExp/judgePoint24.js
@@ -0,0 +1,48 @@
+// You have 4 cards each containing a number from 1 to 9. You need to judge whether they could operated through *, /, +, -, (, ) to get the value of 24.
+
+// Example 1:
+// Input: [4, 1, 8, 7]
+// Output: True
+// Explanation: (8-4) * (7-1) = 24
+// Example 2:
+// Input: [1, 2, 1, 2]
+// Output: False
+// Note:
+// The division operator / represents real division, not integer division. For example, 4 / (1 - 2/3) = 12.
+// Every operation done is between two numbers. In particular, we cannot use - as a unary operator. For example, with [1, 1, 1, 1] as input, the expression -1 - 1 - 1 - 1 is not allowed.
+// You cannot concatenate numbers together. For example, if the input is [1, 2, 1, 2], we cannot write this as 12 + 12.
+
+const judgePoint24 = function(nums) {
+	//converting all the integers to decimal numbers.
+    nums = nums.map(num => Number(num.toFixed(4)));
+    
+	//Function that calculates all possible values after all operations on the numbers passed
+    const computeTwoNums = (num1, num2) => {
+        return [num1 + num2, num1 - num2, num2 - num1, num1 * num2, num1/num2, num2/num1];
+    };
+    
+    const dfs = (list) => {
+        let size = list.length;
+        if(size === 1) {
+            if(Math.abs(list[0] - 24) < 0.001) return true;
+            else return false;
+        }
+        
+        for(let i = 0; i < size; i++) {
+            for(let j = i + 1; j < size; j++) {
+                let nextRound = [];
+                for(let k = 0; k < size; k++) {
+                    if(k !== i && k !== j) nextRound.push(list[k]);
+                }
+                for(let val of computeTwoNums(list[i], list[j])) {
+                    nextRound.push(val);
+                    if(dfs(nextRound)) return true;
+                    else nextRound.pop();
+                }
+            }
+        }
+        return false;
+    };
+    
+    return dfs(nums);
+};
