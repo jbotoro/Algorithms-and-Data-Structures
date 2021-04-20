a2d821da4d4ecd65874ab8e434a164684f9cc034
Jordan Black <jblack530@gmail.com>
2021-04-19 19:31:02 -0700

    contains nearby duplicate algo

---
+++ AlgoExp/containsNearbyDuplicate.js
@@ -0,0 +1,40 @@
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// 0 <= k <= 105
+
+var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+
+        if (map.has(num)) {
+            const j = map.get(num);
+            if (i - j <= k) return true;
+        } 
+        
+        map.set(num, i);
+    }
+    
+    return false;
+};
