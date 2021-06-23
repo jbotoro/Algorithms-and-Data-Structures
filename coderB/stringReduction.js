58cef7c094b1bc4bc8b8ed65960231f0fa03f494
Jordan Black <jblack530@gmail.com>
2021-06-23 16:02:35 -0700

    vowel square coder b algo

--- coderB/stringReduction.js
+++ coderB/stringReduction.js
@@ -33,3 +33,52 @@ var res = str.length + 1;
   return str.length; 
          
 }
+
+
+
+
+const LiveRampDataStoreBase = `data_store_segment_id,name,description,provider,price
+1004765519,123Push > Consumer > Demographic > Senior Citizens > Age 65+,Individuals living in a household with 3 or more people.,123Push,0.75
+1000846336,Alliant > Automotive > Auto - In Market New Vehicle,In-Market model for new vehicle,Alliant,2.75
+1000846346,Alliant > Automotive > Auto - In Market for Insurance,In-Market model for auto insurance,Alliant,2.75
+1000847476,Alliant > Automotive > Auto - In Market for Used Vehicle,In-Market model for used vehicle,Alliant,2.75
+1000846216,Alliant > Demographic > Homeowner,Active multi-channel households who own their home,Alliant,2.0
+1000845656,Alliant > Demographic > Age 25-29 years,Active multi-channel households with a person age 25-29 years old present in the HH.,Alliant,1.75
+1000848136,Alliant > Demographic > Age 30-34 years,Active multi-channel households with a person age 30-34 years old present in the HH.,Alliant,1.75
+1000845646,Alliant > Demographic > Age 35-39 years,Active multi-channel households with a person age 35-39 years old present in the HH.,Alliant,1.75
+1000848146,Alliant > Demographic > Age 40-44 years,Active multi-channel households with a person age 40-44 years old present in the HH.,Alliant,1.75`;
+
+
+function nest(param) {
+    let map = {};
+
+    let splitList = param.split('\n');
+    // console.log(splitList)
+
+    let colArr = splitList[0];
+    splitList.shift();
+
+    for(let i = 0; i < splitList.length; i++){
+        let splitEntry = splitList[i].split(',');
+        let id = splitEntry[0];
+        let nestOrder = splitEntry[1]
+        // console.log(nestOrder);
+        nestOrder = nestOrder.split('>');
+        // console.log(nestOrder);
+        // console.log(nestOrder);
+        let description = splitEntry[2];
+        let provider = splitEntry[3];
+        let price = splitEntry[4];
+        // console.log(splitEntry);
+
+        let mostDepth = nestOrder.length - 1;
+
+        map = nestOrder.reduceRight((obj, next) => ( { [next] : obj } ), {})
+        console.log(map);
+
+       
+    }
+
+}
+
+nest(LiveRampDataStoreBase);
