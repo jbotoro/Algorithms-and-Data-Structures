5d67c45538a48747da24d7a40f14bfb02b3aa2f2
Jordan Black <jblack530@gmail.com>
2021-08-23 16:24:05 -0700

    2nd solution:

--- coderB/timeDifference.js
+++ coderB/timeDifference.js
@@ -16,33 +16,44 @@
 // Output: 16
 
 function TimeDifference(strArr) { 
-
-  function getMins(str){
-    str=str.split(':')
-    if(str[1].match(/[p]/gi) && parseInt(str[0])!==12){
-      str[0]=parseInt(str[0])+12
-    } else if(str[1].match(/[a]/gi)){
-      str[0]=parseInt(str[0])
-    } 
-    if(str[1][0]==='0' && str[1][1]==='0'){
-      str[1]=0
-    } else {
-      str[1]=parseInt(str[1])
+    
+    // empty array to hold times formatted into minutes
+    var timeArr = [];
+    
+    for(var i = 0; i < strArr.length; i++){
+        
+        // cut up time string to make usable for UTC Date method 
+        let strTime = strArr[i];
+        let strTimeEnd = strTime.slice(-2);
+        strTime = strTime.substring(0, strTime.length-2) + " " + strTimeEnd.toUpperCase();
+        
+        // add arbitrary date string plus time for UTC conversion
+        let curr = "6/17/2017 " + strTime;
+        let time = new Date(curr).toUTCString();
+        
+        // remove extra characters not needed - original conversion pre cleanup looks like this: "Sat, 17 Jun 2017 19:20:00 GMT"
+        let cutTime = time.substring(17,time.length-7);
+        cutTime = cutTime.replace(":", "");
+        
+        // convert time string to total minutes as a Number and push to timeArr
+        let minuteConvert = Number(cutTime.substring(0,2) * 60) + Number(cutTime.substring(2,4));
+        timeArr.push(minuteConvert);
     }
-    var mins=(str[0]*60)+str[1]
-    return mins
-  }
-  var arr=[]
-  var lowest=Math.abs(getMins(strArr[0])-getMins(strArr[1]))
-  
-for( var i=0; i<strArr.length; i++){
-  for( var j=i+1; j<strArr.length; j++){
-    var diff=Math.abs(getMins(strArr[i])-getMins(strArr[j]))
-    console.log(lowest, diff)
+
+    // set leastDiff as infinity to guarantee a value less than it
+    var leastDiff = Infinity;
     
-    lowest=Math.min(lowest, diff)
-    
-  }
-}  
-   return lowest    
+    // loop through each index of total minutes and subtract (excluding double times) to get least difference
+    for(var j = 0; j <timeArr.length; j++){
+        for(var m = 0; m < timeArr.length; m++){
+            if(timeArr[j] != timeArr[m]){
+                let diff = Math.abs(timeArr[j] - timeArr[m]);
+                if(diff < leastDiff){
+                    leastDiff = diff;
+                }
+            }
+        }
+    }
+
+  return leastDiff; 
 }
